f2b2ce34fd74b4e0502d34c7c9eeaafd0b3a7d43
BlackJoke007 <134291502+BlackJoke007@users.noreply.github.com>
2024-06-30 03:14:06 +0330

    Update index.php

--- Web.Pishrafteh.Project.MohammadAminSalariEsini/index.php
+++ Web.Pishrafteh.Project.MohammadAminSalariEsini/index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<p style='color: yellow;direction: ltr'>Welcome To My Page</p>";
+echo "<p style='color: yellow;direction: ltr' align='center'>Welcome To My Page</p>";
 include_once "header.php";
 include_once "menu.php";
 include_once "middle.php";
